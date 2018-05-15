6f3eac60c2117408b5bfb64fc2ab229d58f6f592
Tourné <tourne@TrumpetMacbookProTourne.local>
2018-05-15 16:50:21 -0500

    (feat) add footer content + form

--- templates/str-footer.php
+++ templates/str-footer.php
@@ -1,3 +1,14 @@
 <footer class="footer">
-  <div class="test">Lorem Ipsum dolor sit amet</div>
+
+
+  <div class="footer__break">___</div>
+  <h3>Ths is general placeholder text people, don't have a heart attack! Millenium Falcon! Millenium Falcon!</h3>
+
+  <div class="footer__form">
+    <div class="footer__form-container">
+      <?php echo do_shortcode( '[contact-form-7 id="24" title="Footer Contact Form"]' ); ?>
+    </div>
+  </div>
+
+
 </footer>
