46254ae5c1aa8c49ff6624bc7a705e6f81d961d3
Tourné <tourne@TrumpetMacbookProTourne.local>
2018-05-15 17:48:07 -0500

    (feat) add temp logo to footer form, add horiz line break above footer text

--- templates/str-footer.php
+++ templates/str-footer.php
@@ -1,12 +1,13 @@
 <footer class="footer">
 
 
-  <div class="footer__break">___</div>
-  <h3>Ths is general placeholder text people, don't have a heart attack! Millenium Falcon! Millenium Falcon!</h3>
+  <hr class="footer__break"></hr>
+  <h3>Ths is general placeholder text people, don't have a heart attack! Millennium Falcon! Millennium Falcon!</h3>
 
   <div class="footer__form">
     <div class="footer__form-container">
       <?php echo do_shortcode( '[contact-form-7 id="24" title="Footer Contact Form"]' ); ?>
+
     </div>
   </div>
 
